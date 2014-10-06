354caeec11eac986fa262765f34350837b7c728a
ryanmora23 <ryan.mora23@gmail.com>
2014-10-06 14:43:50 -0500

    updating files

---
+++ js/app.js
@@ -0,0 +1,46 @@
+window.onload = app;
+
+function app() {
+    "use strict";
+
+    function time() {
+
+        var d = new Date();
+
+        var seconds = d.getSeconds();
+        var minutes = d.getMinutes();
+        var hours = d.getHours();
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (hours < 10) {
+            hours = "0" + hours;
+
+        }
+        if (hours > 12) {
+            hours = hours - 12;
+            hours = "0" + hours;
+        }
+        var showTime = [hours, minutes, seconds];
+        div.textContent = showTime.join(":");
+        body.style["background-color"] = "rgb(" + convertTimeToRGB(showTime).join(",") + ")";
+    }
+
+    function convertTimeToRGB(timeConvert) {
+
+        var result = [];
+
+        result[0] = ~~((timeConvert[0] / 24) * 256);
+        result[1] = ~~((timeConvert[1] / 60) * 256);
+        result[2] = ~~((timeConvert[2] / 60) * 256);
+
+        return result;
+    }
+    var div = document.querySelector(".clock");
+    var body = document.querySelector("body");
+    setInterval(time, 1000);
+}
